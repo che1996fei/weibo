f7669c9a0926832207e12bdae0ec88e5531931a3
chefei <48946109+che1996fei@users.noreply.github.com>
2019-04-10 22:35:54 +0800

    Add files via upload

--- weibo.py
+++ weibo.py
@@ -6,15 +6,16 @@ base_url = 'https://m.weibo.cn/api/container/getIndex?'
 headers = {
     'Host': 'm.weibo.cn',
     'Referer': 'https://m.weibo.cn/u/2779895470',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest',
 }
 
-def get_page():
+def get_page(page):
     params = {
         'type': 'uid',
         'value': '2779895470',
         'containerid': '1076032779895470',
+        'page': page
     }
     url = base_url + urlencode(params)
     try:
@@ -42,12 +43,8 @@ def parse_page(json):
 
 
 if __name__ == '__main__':
-    json = get_page()
-    results = parse_page(json)
-    for result in results:
-        print(result)
-
-
-
-
-
+    for page in range(1, 48):
+        json = get_page(page)
+        results = parse_page(json)
+        for result in results:
+            print(result)
